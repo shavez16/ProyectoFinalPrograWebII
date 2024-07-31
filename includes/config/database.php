1f1bab8e1a3b6ee30b57225ed371f05cc0925c6a
Eluiffer504 <fredyzzavala7@hotmail.com>
2024-07-30 22:29:23 -0600

    conexion a la base

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,6 +1,6 @@
 <?php
 function conectarDB() : mysqli{
-    $db = mysqli_connect('localhost', 'root', '', 'aqui la base');
+    $db = mysqli_connect('cmsprogramacion.mysql.database.azure.com', 'CMSPROGRAMACIONWEB', 'mdP3@x5@x0Dx', 'base_web2');
 
     if (!$db) {
         echo "Hubo un error al conectar la base de datos: " . mysqli_connect_error();
@@ -8,3 +8,4 @@ function conectarDB() : mysqli{
     } 
     return $db;
 }
+
