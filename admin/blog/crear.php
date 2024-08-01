07d8b0534c6febaae41db35f361c1c941d3c690f
Eluiffer504 <fredyzzavala7@hotmail.com>
2024-07-31 21:27:49 -0600

    Panel admin completado

--- admin/blog/crear.php
+++ admin/blog/crear.php
@@ -1,28 +1,118 @@
+<?php
+
+//Base de datos
+require '../../includes/config/database.php';
+$db = conectarDB();
+
+//Este arreglo me sirve para almacenar errores y despues llamarlos
+$errores = [];
+
+//Declaracion de variables
+$descripcion = '';
+
+//Ejecutar el codigo despues de que el usuario envia el formulario
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //  echo "<pre>";
+    // var_dump($_POST);
+    // echo "</pre>";
+
+    $descripcion = $_POST['descripcion'];
+    
+
+    $creado = date('Y/m/d');
+
+    //Asignar FILE para las imagenes
+    $imagen = $_FILES['imagen'];
+
+    if (strlen($descripcion) < 20) {
+        $errores[] = "La descripcion es obligatoria y debe tener al menos 20 caracteres ";
+    }
+
+    //Aqui valido la imagen
+
+    if (!$imagen['name'] || $imagen['error']) {
+        $errores[] = "La imagen es obligatoria";
+    }
+
+    //Validar el tama;o de la imagen (1mb maximo)
+    $medida = 1000 * 1000;
+    if ($imagen['size'] > $medida) {
+        $errores[] = "La imagen es muy pesada";
+    }
+
+    // Revisar que el arreglo no este vacio
+    if (empty($errores)) {
+
+        //Subida de archivo
+        $carpertaImagenes = '../../imagenes/';
+        if (!is_dir($carpertaImagenes)) {
+            mkdir($carpertaImagenes);
+        }
+        //Generar un nombre unico a la imagen y concatenamos la extencion .jpg
+        $nombreImagen = md5(uniqid(rand(), true)) . ".jpg";
+        //Subir la imagen
+        move_uploaded_file($imagen['tmp_name'], $carpertaImagenes . $nombreImagen);
+
+
+        //Insertar en la base de datos
+        $query = "INSERT INTO publicaciones (descripcion, imagen, creado) 
+              VALUES ('$descripcion', '$nombreImagen','$creado')";
+
+        // echo $query;
+      
+        $resultado = mysqli_query($db, $query);
+        if ($resultado) {
+            //Redireccionar al usuario al formulario anterior
+            header('Location: /admin?resultado=1');
+        }
+    }
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>crear publicacion</title>
     <link rel="stylesheet" href="../../estilos/styleadmin.css">
 </head>
+
 <body>
+
+
     <div class="contenedor">
-    <form action="" method="POST" enctype="multipart/form-data" class="formulario ">
-        <fieldset>
-            <legend>Crear una nueva publicacion</legend>
-            <label for="Descripcion">Descripcion</label>
-            <input type="text" id="Descripcion" name="Descripcion" placeholder="Escribe una descripcion">
-            <label for="imagen">Imagen</label>
-            <input type="file" accept="image/jpeg, image/png" name="imagen">
-        </fieldset>
-        <fieldset>
-            <legend>Guardar</legend>
-        <input type="submit" value="Guardar" class="boton-verde">
-        </fieldset>
-    </form>
+        <h1>Crear</h1>
+        <a href="/admin" class="boton-verde">Volver</a>
+
+        <?php foreach ($errores as $error) : ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+
+        <?php endforeach; ?>
+
+        <form action="/admin/blog/crear.php" method="POST" enctype="multipart/form-data" class="formulario ">
+            <fieldset>
+                <legend>Crear una nueva publicacion</legend>
+                <label for="descripcion">Descripcion</label>
+                <input type="text" id="descripcion" name="descripcion" placeholder="Escribe una descripcion" value="<?php echo $descripcion ?>">
+                <label for="imagen">Imagen</label>
+                <input type="file" accept="image/jpeg, image/png" name="imagen">
+            </fieldset>
+            <fieldset>
+                <legend>Guardar</legend>
+                <input type="submit" value="Guardar" class="boton-verde">
+            </fieldset>
+        </form>
     </div>
-    
-    
+
+
 </body>
+
 </html>
--- admin/index.php
+++ admin/index.php
@@ -2,13 +2,36 @@
 require '../includes/config/database.php';
 
 //importar la conexion
-$base = conectarDB();
+$db = conectarDB();
 
 //realizamos la consulta a la base
 $query = " SELECT * FROM publicaciones";
 
-$resultado = mysqli_query($base, $query);
+$resultado = mysqli_query($db, $query);
 
+//Muestra mensaje condicional
+$result = $_GET['resultado'] ?? null; //busca el get si no existe le agrega un null
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if ($id) {
+
+        //Eliminar la imagen de la propiedad
+        $consulta = "SELECT imagen FROM publicaciones WHERE id = $id";
+        $resultado = mysqli_query($db, $consulta);
+        $publicacion = mysqli_fetch_assoc($resultado);
+        unlink('../imagenes/' . $publicacion['imagen']);
+
+        //Eliminar la propiedad
+        $query = "DELETE FROM publicaciones WHERE id = $id";
+        $resultado = mysqli_query($db, $query);
+        if ($resultado) {
+            header('Location: /admin?resultado=3');
+        }
+    }
+}
 
 // echo "<pre>";
 // var_dump($resultado);
@@ -27,15 +50,25 @@ $resultado = mysqli_query($base, $query);
 
 <body>
     <h1>Administrar tus publicaciones</h1>
+
+    
     
     <div class="contenedor">
+    <?php if (intval($result) === 1) : ?>
+        <p class="alerta exito">Anuncio Creado Correctamente</p>
+    <?php elseif (intval($result) === 2) : ?>
+        <p class="alerta exito">Anuncio Actualizado Correctamente</p>
+    <?php elseif (intval($result) === 3) : ?>
+        <p class="alerta exito">Registro Eliminado Correctamente</p>
+    <?php endif; ?>
     <a class="boton-verde" href="../admin/blog/crear.php">Nueva publicacion</a>
         <table class="tabla1">
             <thead>
-                <tr>
+                <tr class="encabezado">
                     <th>Id</th>
                     <th>Descripcion</th>
                     <th>Imagen</th>
+                    <th>Creado</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
@@ -47,9 +80,13 @@ $resultado = mysqli_query($base, $query);
                         <td>
                             <img src="../imagenes/<?php echo $publicacion["imagen"] ?>" alt="imagen1">
                         </td>
-                        <td>
-                            <a class="boton-anaranjado" href="../admin/blog/actualizar.php?id=<?php echo $publicacion["id"]; ?>">Actualizar</a>
+                        <td><?php echo $publicacion["creado"] ?></td>
+                        <td> 
+                            <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $publicacion['id']; ?>">
                             <button class="boton-rojo" type="submit" value="Eliminar">Eliminar</button>
+                            </form> 
+                            <a class="boton-anaranjado" href="../admin/blog/actualizar.php?id=<?php echo $publicacion["id"]; ?>">Actualizar</a>
                         </td>
                     </tr>
                 <?php endwhile; ?>
